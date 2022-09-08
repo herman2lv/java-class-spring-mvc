edca7b67dbe1363d87b31e9ab47904eb913b6bfd
Herman Rabinkin <herman2lv@gmail.com>
2022-09-08 17:54:29 +0300

    Add sample filter

---
+++ src/main/java/com/hrm/springmvcdemo/web/filter/MyFilter.java
@@ -0,0 +1,19 @@
+package com.hrm.springmvcdemo.web.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class MyFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        System.out.printf("%nFilter: %s, method: %s%n", req.getRequestURI(), req.getMethod());
+        chain.doFilter(req, res);
+    }
+}

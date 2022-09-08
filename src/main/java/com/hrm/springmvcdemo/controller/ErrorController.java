e0018304fb8f5e7d83f3968290e0aef8691f2b48
Herman Rabinkin <herman2lv@gmail.com>
2022-09-08 17:12:43 +0300

    Add sample of exception handler

--- src/main/java/com/hrm/springmvcdemo/controller/ErrorController.java
+++ src/main/java/com/hrm/springmvcdemo/controller/ErrorController.java
@@ -1,7 +1,10 @@
 package com.hrm.springmvcdemo.controller;
 
+import com.hrm.springmvcdemo.service.exception.ApplicationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +15,12 @@ public class ErrorController {
 
     @GetMapping
     public String error() {
+        return "error";
+    }
+
+    @ExceptionHandler
+    public String applicationError(ApplicationException e, Model model) {
+        model.addAttribute("message", e.getMessage());
         return "error";
     }
 }

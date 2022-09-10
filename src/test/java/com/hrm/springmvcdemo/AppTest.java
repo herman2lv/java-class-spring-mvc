9466639067dc4787dbebf37d856e9ae4e9e4a702
Herman Rabinkin <herman2lv@gmail.com>
2022-09-10 15:39:53 +0300

    Add standard context-loads text

---
+++ src/test/java/com/hrm/springmvcdemo/AppTest.java
@@ -0,0 +1,11 @@
+package com.hrm.springmvcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppTest {
+    @Test
+    void contextLoads() {
+    }
+}

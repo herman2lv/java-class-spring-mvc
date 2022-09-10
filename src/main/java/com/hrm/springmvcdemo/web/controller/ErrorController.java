4bc4994f464189405b04f9d7e30e7edbbf6b35f3
Herman Rabinkin <herman2lv@gmail.com>
2022-09-10 14:07:51 +0300

    Modify exception processing

--- src/main/java/com/hrm/springmvcdemo/web/controller/ErrorController.java
+++ src/main/java/com/hrm/springmvcdemo/web/controller/ErrorController.java
@@ -1,12 +1,16 @@
 package com.hrm.springmvcdemo.web.controller;
 
-import com.hrm.springmvcdemo.service.exception.ApplicationException;
+import com.hrm.springmvcdemo.service.exception.AppException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.security.auth.login.AccountNotFoundException;
 
 @Controller
 @ControllerAdvice
@@ -19,7 +23,15 @@ public class ErrorController {
     }
 
     @ExceptionHandler
-    public String applicationError(ApplicationException e, Model model) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String applicationError(AppException e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String applicationError(AccountNotFoundException e, Model model) {
         model.addAttribute("message", e.getMessage());
         return "error";
     }

32eee175d0c4caba799cd6e4c3bbd2e0761637f9
Herman Rabinkin <herman2lv@gmail.com>
2022-09-10 15:39:28 +0300

    Implement Spring Boot configuration

--- src/main/java/com/hrm/springmvcdemo/web/filter/MyFilter.java
+++ src/main/java/com/hrm/springmvcdemo/web/filter/MyFilter.java
@@ -1,14 +1,18 @@
 package com.hrm.springmvcdemo.web.filter;
 
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter("/*")
+//Example of filter registration when url patterns are not specified
+@Component
+@Order(1)
 public class MyFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)

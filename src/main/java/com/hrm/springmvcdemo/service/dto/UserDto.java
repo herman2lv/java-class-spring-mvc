a6a171a0f411b6f491f5e7dd000b3bc080d882b1
Herman Rabinkin <herman2lv@gmail.com>
2023-03-30 22:58:10 +0300

    Add validation constraints to UserDto

--- src/main/java/com/hrm/springmvcdemo/service/dto/UserDto.java
+++ src/main/java/com/hrm/springmvcdemo/service/dto/UserDto.java
@@ -2,6 +2,11 @@ package com.hrm.springmvcdemo.service.dto;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 @RequiredArgsConstructor
@@ -9,8 +14,13 @@ public class UserDto {
 
     private Long id;
 
+    @NotBlank(message = "{validation.user.email.null}")//will be localized by {code{
+    @Email(message = "{validation.user.email.type}")
     private String login;
 
+    @NotBlank //Custom localized message should be used instead of default
+    @Length(min = 8, message = "{validation.user.password.min}")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!_]).{8,}$", message = "{validation.user.password.regexp}")
     private String password;
 
 }

23000bbfa0c4a1b47d4f94e8cce4296d917290d9
herman2lv <herman2lv@gmail.com>
2024-01-23 13:14:35 +0300

    Register filter via web.xml

--- src/main/java/com/hrm/springmvcdemo/web/filter/AuthorizationFilter.java
+++ src/main/java/com/hrm/springmvcdemo/web/filter/AuthorizationFilter.java
@@ -2,17 +2,17 @@ package com.hrm.springmvcdemo.web.filter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/users/getAll", "/users/delete", "/secured/*"})
+//@WebFilter(urlPatterns = {"/users/getAll", "/users/delete", "/secured/*"}) //registration via annotations
 public class AuthorizationFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
             throws IOException, ServletException {
+        System.out.printf("%nFilter [Security]: %s, method: %s%n", req.getRequestURI(), req.getMethod());
         Object user = req.getSession().getAttribute("user");
         if (user == null) {
             res.sendRedirect("/login");
--- src/main/java/com/hrm/springmvcdemo/web/filter/MyFilter.java
+++ src/main/java/com/hrm/springmvcdemo/web/filter/MyFilter.java
@@ -2,13 +2,12 @@ package com.hrm.springmvcdemo.web.filter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter("/*")
+//@WebFilter("/*") //registration via annotations
 public class MyFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
